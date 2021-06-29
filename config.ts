13bb192de104d8a4c2ffcc4c317067882d246ae6
Miguel Pazo <miguel.ps19@gmail.com>
2021-06-28 22:39:09 -0500

    update param certificateArn

--- config.ts
+++ config.ts
@@ -9,17 +9,4 @@ export const mainBucket = `${cdnName}-bucket`;
 export const suffix = `-${pulumi.getStack()}`;
 export const generalTagName = cdnName || "deploy_cdn";
 export const targetDomain = configPulumi.get("targetDomain");
-
-/**
- * Fetching certificate for target domain for CDN
- */
-const referenceCerts = configPulumi.get("referenceCerts");
-const certificates = new pulumi.StackReference(`${referenceCerts}`);
-
-export const certificateArn = pulumi.output(certificates.getOutput("certificates")).apply(x => {
-    for (let i in x) {
-        if (x[i].domain == targetDomain) {
-            return x[i].certificateArn;
-        }
-    }
-});
+export const certificateArn = configPulumi.get("certificateArn");
